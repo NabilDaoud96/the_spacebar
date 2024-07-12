0d760b067a364d2326cafd1c766fe6f928980093
nabil daoud <nabildaoud45@gmail.com>
2024-07-12 13:56:23 +0200

    intial commit

---
+++ src/Twig/AppExtentionExtension.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Twig;
+
+use App\Service\MarkdownHelper;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ServiceSubscriberInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtentionExtension extends AbstractExtension implements ServiceSubscriberInterface
+{
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public  function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('cached_markdown', [$this, 'processMarkdown'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function processMarkdown($value)
+    {
+        return $this->container
+            ->get(MarkdownHelper::class)
+            ->parse($value);
+    }
+
+    public static function getSubscribedServices()
+    {
+        return[
+            MarkdownHelper::class
+        ];
+    }
+
+
+}
